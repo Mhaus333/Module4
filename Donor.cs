88a5a673478dffaeac66f99195a4bb05125c3410
Mhaus333 <mhouse@my.dom.edu>
2024-09-23 21:45:07 -0500

    Add files via upload

---
+++ Donor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Module_IV_API
+{
+	public class Donor
+	{
+ 
+        public string MedicalRecordNumber { get; set; }
+        public string FirstName { get; set; }
+        public string BloodType { get; set; }
+        public string LastName { get; set; }
+
+
+        public Donor()
+        {
+        }
+
+        public Donor(string medicalRecordNumber, string firstName, string bloodType, string lastName)
+        {
+            this.MedicalRecordNumber = medicalRecordNumber;
+            this.FirstName = firstName;
+            this.BloodType = bloodType;
+            this.LastName = lastName;
+        }
+    }
+    }
+	
+
+
---
+++ DonorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Module_IV_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DonorController : ControllerBase
+    {
+
+        public List<Donor> GetDonors()
+        {
+            List<Donor> donors = new List<Donor>(4);
+
+            donors.Add(new Donor("Michael", "House", "O Positive", "MRN:2783389"));
+            donors.Add(new Donor("Celeste", "Iglesias", "O Positive", "MRN:2344389"));
+            donors.Add(new Donor("Galilea", "Ramirez", "O Positive", "MRN:4754389"));
+            donors.Add(new Donor("Onessimo", "Marillo", "O Positive", "MRN:8643389"));
+
+            return donors;
+        }
+    }
+}
